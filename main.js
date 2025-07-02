22cadf81c9f5a3c66908b84f18c3094db39ed2b3
Tue Sandbæk <tue@stvp.dk>
2025-07-02 19:43:09 +0200

    design changes

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 722,
-    height: 375,
+    width: 738,
+    height: 404,
     webPreferences: {
       preload: path.join(__dirname, 'src/renderer.js'),
       contextIsolation: false,
       nodeIntegration: true
     }
   });
+
+  // win.webContents.openDevTools(); // Debug
 
   win.loadFile(path.join(__dirname, 'src/renderer.html'));
 }
